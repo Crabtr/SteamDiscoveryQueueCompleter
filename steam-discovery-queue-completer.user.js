5714787a7042a4d656a41dd4511b2c14f9aaa262
Evan <evan.crabtr@gmail.com>
2017-06-23 20:52:15 -0400

    Handle age check and gate pages

--- steam-discovery-queue-completer.user.js
+++ steam-discovery-queue-completer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Steam Discovery Queue Completer
-// @version      1.1.1
+// @version      1.2.0
 // @description  Automatically completes the Steam discovery queue
 // @license      GPLv3
 // @author       Evan Crabtree <github.com/crabtr>
@@ -11,8 +11,6 @@
 // @downloadURL https://raw.githubusercontent.com/Crabtr/SteamDiscoveryQueueCompleter/master/steam-discovery-queue-completer.user.js
 // ==/UserScript==
 
-
-
 document.onload = (function() {
     'use strict';
 
@@ -21,6 +19,51 @@ document.onload = (function() {
 
     if (buttons.length > 0) {
         buttons[0].click();
+    }
+
+    // Determine if the page is an age check or an age gate
+    // Age check pages contain a "Don't show this warning again" checkbox while
+    // age gate pages don't
+    let remember = document.getElementById("remember"); // Warning checkbox
+
+    if (remember) {
+        remember.checked = true;
+
+        buttons = document.getElementsByClassName("btn_grey_white_innerfade btn_medium");
+
+        if (buttons.length > 0) {
+            let innerText = buttons[0].innerText.trim();
+
+            if (innerText === "Continue") {
+                buttons[0].click();
+            }
+        }
+    } else {
+        buttons = document.getElementsByClassName("btnv6_blue_hoverfade btn_small");
+
+        if (buttons.length > 1) {
+            let innerText = buttons[0].innerText.trim();
+
+            if (innerText === "Enter")  {
+                // Sets your age to "1 January 1990"
+                let ageButtons = document.getElementsByName("ageDay");
+                if (ageButtons.length > 0) {
+                    ageButtons[0].value = "1";
+                }
+
+                ageButtons = document.getElementsByName("ageMonth");
+                if (ageButtons.length > 0) {
+                    ageButtons[0].value = "January";
+                }
+
+                ageButtons = document.getElementsByName("ageYear");
+                if (ageButtons.length > 0) {
+                    ageButtons[0].value = "1990";
+                }
+
+                buttons[0].click();
+            }
+        }
     }
 
     // Start a new queue after completion
